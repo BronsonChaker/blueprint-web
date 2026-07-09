02d0b00fcd55bb24e2cad903d7a11b1a0e2809e9
Bronson Chaker <50012659+BronsonChaker@users.noreply.github.com>
2026-07-09 20:53:47 +1000

    Changes made to Navbar and Jobs page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,28 +2,32 @@ import { Link, NavLink } from "react-router";
 
 export default function Navbar() {
   return (
-    <nav className="h-18 w-full py-5 px-10 flex flex-row items-center border-b border-stone-300 shadow-sm">
+    <nav className="h-18 w-full py-5 px-10 flex flex-row items-center">
       {/* Logo */}
-      <div className="w-1/4 justify-center">
-        <Link to="">
-          <span className="font-bold text-3xl text-primary">Blueprint</span>
-        </Link>
-      </div>
 
       {/* Links */}
 
-      <div className="w-2/4 flex justify-between font-semibold text-stone-500 text-lg">
+      <div className="w-2/4 flex font-semibold text-stone-500 text-md gap-15 items-center">
+        <Link to="">
+          <span className="font-bold text-3xl text-primary">B</span>
+        </Link>
+
         <NavLink
           className={({ isActive }) => (isActive ? "selected" : "")}
           to="/dashboard"
         >
-          Dashboard
+          <div className="flex flex-row gap-1 items-center font-bold">
+            <p>Dashboard</p>
+          </div>
         </NavLink>
         <NavLink
           className={({ isActive }) => (isActive ? "selected" : "")}
           to="/jobs"
         >
-          Jobs
+          <div className="flex flex-row gap-1 items-center bold text-stone-600 hover:bg-stone-100 rounded-md p-1 hover:duration-200">
+            <span class="material-symbols-outlined">workspaces</span>
+            <p>Jobs</p>
+          </div>
         </NavLink>
         <NavLink
           className={({ isActive }) => (isActive ? "selected" : "")}
--- src/pages/Jobs.jsx
+++ src/pages/Jobs.jsx
@@ -1,7 +1,9 @@
 export default function Jobs() {
   let job_count = 21;
+  let supervisor = "Bronson Chaker";
+  let address = "123 Proposed Road, Silverdale, NSW, 2749";
   return (
-    <div className="p-10 h-screen w-full bg-neutral">
+    <div className="p-20 h-screen w-full ">
       <title>Projects</title>
       {/* Top Header/Card Section */}
       <div className="w-full flex flex-row">
@@ -21,87 +23,77 @@ export default function Jobs() {
             <span class="text-lg font-semibold text-white">+</span>
             <span class="text-lg font-semibold text-white">New Project</span>
           </button>
+          <button className="bg-primary h-3/4 flex gap-2 justify-center items-center px-5 rounded-sm hover:cursor-pointer hover:opacity-80 hover:duration-250">
+            <span class="text-lg font-semibold text-white">+</span>
+            <span class="text-lg font-semibold text-white">SELECT</span>
+          </button>
         </div>
       </div>
 
       {/* Project Table Section */}
       <div className="w-full mt-15">
-        <table className="w-full">
-          <thead className="w-full justify-between content-between rounded-lg shadow-sm bg-neutral">
-            {/* Filters, Drop Downs, and Menus */}
-            <tr>
-              <td>
-                <form class="filter">
-                  <input
-                    class="btn"
-                    type="checkbox"
-                    name="frameworks"
-                    aria-label="Svelte"
-                  />
-                  <input
-                    class="btn"
-                    type="checkbox"
-                    name="frameworks"
-                    aria-label="Vue"
-                  />
-                  <input
-                    class="btn"
-                    type="checkbox"
-                    name="frameworks"
-                    aria-label="React"
-                  />
-                  <input class="btn btn-square" type="reset" value="×" />
-                </form>
-              </td>
-            </tr>
-
+        <table className="w-full  border-gray-300 bg-stone-100 shadow-xs">
+          <thead className="w-full  bg-primary text-white justify-between content-between">
             {/* Table Header Columns */}
             <tr className="w-full">
               <th
                 scope="col"
-                className="w-[30%] text-start p-5 text-lg font-semibold"
+                className="w-[30%] text-start p-5 text-lg font-medium"
               >
-                PROJECT
+                Project
               </th>
 
               <th
                 scope="col"
-                className="w-[15%] text-start p-5 text-lg font-semibold"
+                className="w-[15%] text-start p-5 text-lg font-medium"
               >
-                SUPERVISOR
+                Supervisor
               </th>
               <th
                 scope="col"
-                className="w-[15%] text-start p-5 text-lg font-semibold"
+                className="w-[15%] text-start p-5 text-lg font-medium"
               >
-                STAGE
+                Stage
               </th>
               <th
                 scope="col"
-                className="w-[20%] text-start p-5 text-lg font-semibold"
+                className="w-[20%] text-start p-5 text-lg font-medium"
               >
-                PROGRESSION
+                Progression
               </th>
               <th
                 scope="col"
-                className="w-[20%] text-start p-5 text-lg font-semibold"
+                className="w-[20%] text-start p-5 text-lg font-medium"
               >
-                ESTIMATED COMPLETION
+                Estimated Completion
               </th>
             </tr>
           </thead>
           <tbody>
-            <tr className="text-xl hover:cursor-pointer hover:opacity-75 hover:bg-stone-200 hover:duration-200 rounded-md">
-              <td className="p-5 ">123 Proposed Road, Silverdale</td>
-              <td className="flex flex-row items-center p-5 gap-2">
-                <img className="w-5 h-5 rounded-full"></img>
-                <p>Bronson Chaker</p>
+            <tr className="border-2 border-gray-300 hover:cursor-pointer hover:opacity-90 hover:bg-stone-200 hover:duration-200">
+              <td className="p-5 font-semibold text-lg text-stone-700 ">
+                {address}
               </td>
-              <td className="p-5">Completion</td>
-              <td className="p-5">45% Completed</td>
-              <td className="p-5 flex flex-row items-center gap-2">
-                <span class="material-symbols-outlined">calendar_clock</span>Oct
-                26, 2026
+              <td className=" items-center p-5 ">
+                <div className="w-full h-full flex flex-row items-center gap-2">
+                  <span className="p-2 bg-blue-300 rounded-full">BC </span>
+                  <p>{supervisor}</p>
+                </div>
+              </td>
+              <td className="p-5">Finalising</td>
+              <td className="p-5 ">
+                <p>70% Completed</p>
+                <progress
+                  class="progress progress-primary w-56"
+                  value="70"
+                  max="100"
+                ></progress>
+              </td>
+              <td className="p-5">
+                <div className="w-full h-full flex flex-row gap-2">
+                  <span class="material-symbols-outlined">calendar_clock</span>
+                  <p>Oct 26, 2026</p>
+                </div>
               </td>
             </tr>
           </tbody>
