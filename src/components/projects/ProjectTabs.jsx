32ea9d82b2d390784a19539ded082fe30e2ee28d
Bronson Chaker <50012659+BronsonChaker@users.noreply.github.com>
2026-07-27 06:09:41 +1000

    Created Tabs sect ion in project overview skeleton only done no styline or proper data serving, still to be checked p[otedntally to be changed to materialUI tabs

---
+++ src/components/projects/ProjectTabs.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import Tab from "./Tab";
+
+export default function ProjetTabs({ tabs }) {
+  const [activeTab, setActiveTab] = useState(1);
+
+  const handleTabClick = (index) => {
+    setActiveTab(index + 1);
+  };
+  return (
+    <div className="tabs-container">
+      <div className="tabs">
+        {tabs.map((tab, index) => (
+          <Tab
+            key={index}
+            label={tab.label}
+            onClick={() => handleTabClick(index)}
+            isActive={index === activeTab}
+          />
+        ))}
+      </div>
+      <div className="tab-content">Tab {activeTab} is Active</div>
+    </div>
+  );
+}
