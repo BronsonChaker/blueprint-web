20389358c6dfb589029cb10e67f6ff36b9b7259d
Bronson Chaker <50012659+BronsonChaker@users.noreply.github.com>
2026-08-30 08:14:52 +1000

    svar gantt added to project config and functionality still required

--- src/pages/Planner.jsx
+++ src/pages/Planner.jsx
@@ -1,12 +1,73 @@
+import { Gantt, Willow, Editor, Toolbar } from "@svar-ui/react-gantt";
+import { useState } from "react";
+import "@svar-ui/react-gantt/all.css";
+
+const tasks = [
+  {
+    id: 20,
+    text: "New Task",
+    start: new Date(2026, 5, 11),
+    end: new Date(2026, 6, 12),
+    duration: 1,
+    progress: 2,
+    type: "task",
+    lazy: false,
+  },
+  {
+    id: 47,
+    text: "[1] Master project",
+    start: new Date(2026, 5, 12),
+    end: new Date(2026, 7, 12),
+    duration: 8,
+    progress: 0,
+    parent: 0,
+    type: "summary",
+  },
+  {
+    id: 22,
+    text: "Task",
+    start: new Date(2026, 7, 11),
+    end: new Date(2026, 8, 12),
+    duration: 8,
+    progress: 0,
+    parent: 47,
+    type: "task",
+  },
+  {
+    id: 21,
+    text: "New Task 2",
+    start: new Date(2026, 7, 10),
+    end: new Date(2026, 8, 12),
+    duration: 3,
+    progress: 0,
+    type: "task",
+    lazy: false,
+  },
+];
+const links = [{ id: 1, source: 20, target: 21, type: "e2e" }];
+
+const scales = [
+  { unit: "month", step: 1, format: "%F %Y" },
+  { unit: "day", step: 1, format: "%j" },
+];
+
 export default function Planner() {
+  const [api, setApi] = useState(null);
   return (
-    <div className="px-10 py-5 min-h-screen">
+    <div className="px-10 py-5">
       <title>Planner</title>
       <div className=" w-full">
         <h1 className="text-heading">Planner</h1>
         <p className="text-page-desc">
           Use the planner to visually inspect timelines and make adjustments
         </p>
+      </div>
+      <div className="w-full h-full">
+        <Willow>
+          <Toolbar api={api} />
+          <Gantt tasks={tasks} links={links} scales={scales} zoom={true} />
+          {api && <Editor api={api} />}
+        </Willow>
       </div>
     </div>
   );
